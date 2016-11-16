7045d8c83b0a05ab54390783499ba1c146b39b6d
Brian Hill <brian@lingerhere.org>
2016-11-16 13:17:13 -0800

    Fix bugs introduced when I made the stochastic perturbation generate vectors of tuples.

--- stochastic_perturbation.py
+++ stochastic_perturbation.py
@@ -3,8 +3,7 @@ from random import gauss
 
 from data_from_ng import N, N_M, N_U
 
-# I am very concerned that values too close to zero will result in precision problems
-
+# I am concerned that perturbations too close to zero will result in precision problems
 TOO_SMALL = 0.00001
 
 
@@ -17,21 +16,21 @@ def generate_scalar_perturbation(sigma):
     return result
 
 
-# This does a tuple
+# This does a tuple of length N
 def generate_tuple_perturbation(sigma):
     return tuple([generate_scalar_perturbation(sigma) for _ in range(0, N)])
 
 
 # This does N_M tuples
 def generate_x_perturbation(sigma):
-    # number of components in x is N_M
-    return [generate_scalar_perturbation(sigma) for _ in range(0, N_M)]
+    # number of components in x is N_M, and each component is a tuple of length N
+    return [generate_tuple_perturbation(sigma) for _ in range(0, N_M)]
 
 
 # This does N_U tuples
 def generate_theta_perturbation(sigma):
-    # number of components in theta is N_U
-    return [generate_perturbation(sigma) for _ in range(0, N_U)]
+    # number of components in theta is N_U, and each component is a tuple of length N
+    return [generate_tuple_perturbation(sigma) for _ in range(0, N_U)]
 
 
 # This does a whole perturbation all at once for all of the (N_M + N_U) * N components
