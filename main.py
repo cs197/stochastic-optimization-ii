deea89975a6eb2f271e918db926a184bb0121d43
Brian Hill <brian@lingerhere.org>
2016-11-18 14:59:00 -0800

    Added an implementation of Spall's a_k and c_k (the gain factors).

--- main.py
+++ main.py
@@ -10,6 +10,25 @@ from tuple_operations import vector_sum, vector_multiply
 
 from random import seed
 
+from math import exp, log
+
+# Spall's gain parameters
+
+gain_a_multiplier = 0.01
+gain_a_fudge = 2.0
+alpha = 0.2
+
+
+def gain_a(idx):
+    return gain_a_multiplier / exp(alpha * log(idx + 1 + gain_a_fudge))
+
+gain_c_multiplier = 0.01
+gamma = 0.2
+
+
+def gain_c(idx):
+    return gain_c_multiplier / exp(gamma * log(idx + 1))
+
 
 # In simultaneous_perturbation.py, you can implement Spall's algorithm.
 #
@@ -22,12 +41,12 @@ def optimize_ng_example():
     lamb = 1.0  # I'd call this lambda, except in Python lambda is a keyword. Initialize to 1.0. What should it be?
     optimization_objective_function = make_optimization_objective(lamb, REVIEWS)
 
-    gain_a = 1.0  # One of Spall's gain factors. Should be decreased as the optimization converges.
-    gain_c = 1.0  # The other of Spall's gain factors. Also should be decreased as the optimization converges.
     convergence_criterion = 0.0001
 
     # Our initial guess -- totally random -- no method or logic here.
-    x_list, theta_list = generate_perturbation(gain_a)
+    idx = 1
+    c = gain_c(idx)
+    x_list, theta_list = generate_perturbation(c)
     optimization_objective_value = optimization_objective_function(x_list, theta_list)
 
     print "Initially, the optimization objective is " + str(optimization_objective_value)
